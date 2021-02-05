0320dbf11951c6a4a9e154f39a641838ab5ffe47
tiago0022 <tiago.araujo.neo@gmail.com>
2021-02-05 14:42:01 -0300

    ajuste parametro

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 from estrutura_dados.leitor_entrada import obtem_instancia
 
-caminho_instancia = 'instancias/basico'
+caminho_instancia = 'instancias/basico/'
 
 if len(sys.argv) >= 2:
-    caminho_instancia = sys.argv[1]
+    caminho_instancia = sys.argv[1][:-1]
 
 df_anuncio, df_conflito, tamanho_quadro, quantidade_quadros = obtem_instancia(caminho_instancia)
 
-# print(f"Anúncios A_i:\n{dfAnuncio}\n\nConflitos C_ij:\n{dfConflito}\n\nTamanho do quadro L: {tamanhoQuadro}\nQuantidade de quadros B: {quantidadeQuadros}\n")
+print(f"Tamanho do quadro L: {tamanho_quadro}\nQuantidade de quadros B: {quantidade_quadros}\n\nAnúncios A_i:\n{df_anuncio}\n\nConflitos C_ij:\n{df_conflito}\n")
