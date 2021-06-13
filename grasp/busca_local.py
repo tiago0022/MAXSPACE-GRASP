f247509b6b3dd7d7dd6a32a501e55e5cd0bf1c4b
tiago0022 <tiago.araujo.neo@gmail.com>
2021-06-13 01:15:32 -0300

    vizinhancas separadas

--- grasp/busca_local.py
+++ grasp/busca_local.py
@@ -30,33 +30,70 @@ class BuscaLocal:
 
     def _obtem_melhor_vizinho(self, solucao: Solucao) -> Solucao:
 
-        for disponivel in solucao.lista_anuncio_disponivel:
+        solucao_adiciona = self._melhor_vizinho_adiciona(solucao)
+        if solucao_adiciona != None:
+            return solucao_adiciona
 
-            solucao_adiciona = solucao.adiciona(disponivel)
-            if solucao_adiciona != None:
-                print(disponivel, 'adicionado')
-                return solucao_adiciona
+        solucao_substitui = self._melhor_vizinho_substitui(solucao)
+        if solucao_substitui != None:
+            return solucao_substitui
 
-            for adicionado in solucao.lista_anuncio_adicionado:
-                solucao_substitui = solucao.substitui(adicionado, disponivel)
-                if solucao_substitui != None and solucao_substitui.ehMelhor(solucao):
-                    print(adicionado, 'removido e', disponivel, 'adicionado')
-                    return solucao_substitui
+        solucao_move = self._melhor_vizinho_move(solucao)
+        if solucao_move != None:
+            return solucao_move
 
+        solucao_remaneja = self._melhor_vizinho_remaneja(solucao)
+        if solucao_remaneja != None:
+            return solucao_remaneja
+
+        return None
+
+    def _melhor_vizinho_adiciona(self, solucao: Solucao) -> Solucao:
+        melhor = solucao
+        melhor_encontrado = False
+        for i in solucao.lista_anuncio_disponivel:
+            solucao_adiciona = solucao.adiciona(i)
+            if solucao_adiciona != None and solucao_adiciona.ehMelhor(melhor):
+                print(i, 'adicionado')
+                melhor = solucao_adiciona
+                melhor_encontrado = True
+        return melhor if melhor_encontrado else None
+
+    def _melhor_vizinho_substitui(self, solucao: Solucao) -> Solucao:
+        melhor = solucao
+        melhor_encontrado = False
+        for i in solucao.lista_anuncio_adicionado:
+            for j in solucao.lista_anuncio_disponivel:
+                solucao_substitui = solucao.substitui(i, j)
+                if solucao_substitui != None and solucao_substitui.ehMelhor(melhor):
+                    print(i, 'removido e', j, 'adicionado')
+                    melhor = solucao_substitui
+                    melhor_encontrado = True
+        return melhor if melhor_encontrado else None
+
+    def _melhor_vizinho_move(self, solucao: Solucao) -> Solucao:
+        melhor = solucao
+        melhor_encontrado = False
         for i in solucao.lista_anuncio_adicionado:
             for quadro_i in solucao.matriz_anuncio_quadro[i]:
-
                 for quadro_k in solucao.lista_quadro_disponivel:
                     solucao_move = solucao.move(i, quadro_i, quadro_k)
-                    if solucao_move != None and solucao_move.ehMelhor(solucao):
+                    if solucao_move != None and solucao_move.ehMelhor(melhor):
                         print(i, 'no quadro', quadro_i, 'movido para o quadro', quadro_k)
-                        return solucao_move
+                        melhor = solucao_move
+                        melhor_encontrado = True
+        return melhor if melhor_encontrado else None
 
+    def _melhor_vizinho_remaneja(self, solucao: Solucao) -> Solucao:
+        melhor = solucao
+        melhor_encontrado = False
+        for i in solucao.lista_anuncio_adicionado:
+            for quadro_i in solucao.matriz_anuncio_quadro[i]:
                 for j in solucao.lista_anuncio_adicionado:
                     for quadro_j in solucao.matriz_anuncio_quadro[j]:
                         solucao_remaneja = solucao.remaneja(i, quadro_i, j, quadro_j)
-                        if solucao_remaneja != None and solucao_remaneja.ehMelhor(solucao):
+                        if solucao_remaneja != None and solucao_remaneja.ehMelhor(melhor):
                             print(i, 'no quadro', quadro_i, 'trocado com', j, 'do quadro', quadro_j)
-                            return solucao_remaneja
-
-        return None
+                            melhor = solucao_remaneja
+                            melhor_encontrado = True
+        return melhor if melhor_encontrado else None
